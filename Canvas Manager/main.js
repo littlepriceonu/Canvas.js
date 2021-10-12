b828df5fdb7b8430f2399b698dea95cca3e5dfaf
littlepriceonu <littlepriceonu@icloud.com>
2021-10-11 21:10:21 -0700

    added 'fillStyle' for when updates

--- Canvas Manager/main.js
+++ Canvas Manager/main.js
@@ -1,19 +1,23 @@
 
 
 class CanvasManager {
-    constructor (Canvas, updateCallBack, clearOnUpdate, paused, updateSpeed) {
+    constructor (Canvas, updateCallBack, clearOnUpdate, paused, updateSpeed, fillStyle) {
         this.canvas = Canvas
         this.ctx = Canvas.getContext("2d");
         this.updatecallback = updateCallBack;
         this.clearonupdate = clearOnUpdate;
         this.paused = paused
         this.updatespeed = updateSpeed;
+        this.fillstyle = fillStyle
     }
 
     worldUpdate() {
         if (!this.paused) {
             if (this.clearonupdate) {
+                this.ctx.fillStyle = this.fillstyle
                 this.ctx.clearRect(0, 0, innerWidth, innerHeight)
+                this.ctx.fillRect(0, 0, innerWidth, innerHeight);
+                
             }
 
             for (let i=0; i < this.updatecallback.length; i++) {
